49ac8c91a592ba7cc679e08fabc83eb802f5318d
Jminss <yjm0243@naver.com>
2022-12-07 15:08:47 +0900

    init commit

---
+++ wine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wine
+//
+//  Created by 2017261069 윤재민 on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
