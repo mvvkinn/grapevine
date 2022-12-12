ceed73c83a156b146a47a654064d9550fd159b1c
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-12-12 23:00:08 +0900

    Fix formatting

--- Predictors/isWinePredictor.swift
+++ Predictors/isWinePredictor.swift
@@ -15,57 +15,57 @@ class isWinePredictor {
     static func createClassifier() -> VNCoreMLModel {
         // Use a default model configuration.
         let defaultConfig = MLModelConfiguration()
-
+        
         // Create an instance of the image classifier's wrapper class.
         let isWineClassifierWrapper = try? isWineClassifier(configuration: defaultConfig)
-
+        
         guard let isWineClassifier = isWineClassifierWrapper else {
             fatalError("App failed to create an image classifier model instance.")
         }
-
+        
         // Get the underlying model instance.
         let isWineClassifierModel = isWineClassifier.model
-
+        
         // Create a Vision instance using the image classifier's model instance.
         guard let isWineClassifierVisionModel = try? VNCoreMLModel(for: isWineClassifierModel) else {
             fatalError("App failed to create a `VNCoreMLModel` instance.")
         }
-
+        
         return isWineClassifierVisionModel
     }
     
     private static let imageClassifier = createClassifier()
-
+    
     struct Prediction {
         let classification: String
         let confidencePercentage: String
     }
     
     typealias ImagePredictionHandler = (_ predictions: [Prediction]?) -> Void
-
+    
     private var predictionHandlers = [VNRequest: ImagePredictionHandler]()
-
+    
     private func createImageClassificationRequest() -> VNImageBasedRequest {
         let imageClassificationRequest = VNCoreMLRequest(model: isWinePredictor.imageClassifier,
                                                          completionHandler: visionRequestHandler)
-
+        
         imageClassificationRequest.imageCropAndScaleOption = .centerCrop
         return imageClassificationRequest
     }
     
     func makePredictions(for photo: UIImage, completionHandler: @escaping ImagePredictionHandler) throws {
         let orientation = CGImagePropertyOrientation(photo.imageOrientation)
-
+        
         guard let photoImage = photo.cgImage else {
             fatalError("Photo doesn't have underlying CGImage.")
         }
-
+        
         let imageClassificationRequest = createImageClassificationRequest()
         predictionHandlers[imageClassificationRequest] = completionHandler
-
+        
         let handler = VNImageRequestHandler(cgImage: photoImage, orientation: orientation)
         let requests: [VNRequest] = [imageClassificationRequest]
-
+        
         try handler.perform(requests)
     }
     
