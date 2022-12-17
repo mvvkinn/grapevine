03c44464ec83183918fa82ab6d713223e1811420
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-12-17 21:25:29 +0900

    Add model Wine

---
+++ Component/Models/Wine.swift
@@ -0,0 +1,16 @@
+//
+//  Wine.swift
+//  Grapevine
+//
+//  Created by 김민우 on 2022/12/17.
+//
+
+import Foundation
+
+struct Wine {
+    let name: String
+    let country: String
+    let alcoholPercentage: Float16
+    let features: String
+}
+
